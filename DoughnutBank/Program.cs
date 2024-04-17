36a214b5d9e60fa65c7234eb160db29151fecf5f
Alexandru Pop <alex99_pop@yahoo.com>
2024-04-17 15:21:30 +0300

    add authorization button in swagger

--- DoughnutBank/Program.cs
+++ DoughnutBank/Program.cs
@@ -1,6 +1,8 @@
 using DoughnutBank.Authentication.ApiAccess;
 using DoughnutBank.Services.Implementations;
 using DoughnutBank.Services.Interfaces;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => addAuthButtonForSwaggerThroughOptions(options));
+void addAuthButtonForSwaggerThroughOptions(SwaggerGenOptions options)
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "The API key to access the API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+
+    var requirement = new OpenApiSecurityRequirement
+    {
+        {scheme, new List<string>() }
+    };
+
+    options.AddSecurityRequirement(requirement);
+}
+
 builder.Services.AddSingleton<IOTPGenerator, OTPCryptoGenerator>();
 builder.Services.AddTransient<ApiAccessMiddleware>();
 
