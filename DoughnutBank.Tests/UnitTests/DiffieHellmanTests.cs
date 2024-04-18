8511592113002f25e53ab3bf6c41e2f02978e615
Alexandru Pop <alex99_pop@yahoo.com>
2024-04-18 14:08:05 +0300

    add DiffieHellman class and tests, add authentication service, add signup in controller

---
+++ DoughnutBank.Tests/UnitTests/DiffieHellmanTests.cs
@@ -0,0 +1,55 @@
+﻿using DoughnutBank.Utils;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoughnutBank.Tests.UnitTests
+{
+    public class DiffieHellmanTests
+    {
+        [Fact]
+        public void Encrypt_encryptedMessageShouldBeDifferentFromOriginal()
+        {
+            //arrange
+            var messageToEncrypt = "This is just a random message";
+            var diffieHellmanOtherParty = new DiffieHellman();
+            var otherPartyPublicKey = diffieHellmanOtherParty.PublicKey;
+
+            //act
+            var diffieHellman = new DiffieHellman();
+            var encryptedMessageAsByteArray = diffieHellman.Encrypt(otherPartyPublicKey, messageToEncrypt);
+
+            //assert
+            var areEquals = encryptedMessageAsByteArray.Equals(messageToEncrypt);
+            areEquals.Should().Be(false);
+        }
+
+        [Theory]
+        [InlineData("ThisIsJustAMessage")]
+        [InlineData("I am doing this alone")]
+        [InlineData("980 675")]
+        public void EncryptDecrypt_encryptedMessageShouldAllowToBeDecrypted(string messageAliceEncryts)
+        {
+            //arrange
+            var BobDiffieHellman = new DiffieHellman();
+            var BobPublicKey = BobDiffieHellman.PublicKey;
+
+            var AliceDiffieHellman = new DiffieHellman();
+            var AlicePublicKey = AliceDiffieHellman.PublicKey;
+            var AliceIV = AliceDiffieHellman.IV;
+
+            //act
+            var encryptedMessageByAlice = AliceDiffieHellman.Encrypt(BobPublicKey, messageAliceEncryts);
+
+            string decryptedMessageByBob = BobDiffieHellman.Decrypt(AlicePublicKey, encryptedMessageByAlice, AliceIV);
+
+
+            //assert
+            var areEquals = messageAliceEncryts.Equals(decryptedMessageByBob);
+            areEquals.Should().Be(true);
+        }
+    }
+}
